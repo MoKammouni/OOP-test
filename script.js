92a13c0ff4c16e0bf32d64fa6dd6e40012555018
MoKammouni <57012891+MoKammouni@users.noreply.github.com>
2020-02-12 22:07:28 +0100

    Eerste documentatie van OOP Javascript course

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// Dit word ook wel Object Oriented Syntax genoend en is een slecht voorbeeld
+// Als een object 1 of meer methodes heeft dan noemen we dat behavior(gedrag)
+// const circle = {
+//    radius: 1,
+//    location: {
+//        x: 1, Dit worden in OOP properties oftewel eigenschappen genoemd
+//        y:1
+//    },
+//    draw: function() {  In OOP word een function een method genoemd dus ook in deze instantie is het een method
+//        console.log('draw');
+//    }
+// };
+
+// Dit wordt een Factory Function genoemd
+// Als wij de return keyword gebruiken dan heet dat een Factory Function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function(){
+            console.log('draw');
+        }
+    };
+}
+const circle = createCircle(1);
+
+// Constructor Function
+// In een Constructor Function moet de functie naam altijd beginnen met een hoofdletter
+// Als je de this key word gebruikt en de new operator dan noemen wij die functie een Constructor functie
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+const another = new Circle(1);
