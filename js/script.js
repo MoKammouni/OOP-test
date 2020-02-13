8ae019192cac550d1a295c1472a408d520ecaae5
Mohamed el Kammouni <mokammouni@outlook.com>
2020-02-13 20:07:34 +0100

    Verandering plaats gevonden

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 
-// Dit word ook wel Object Oriented Syntax genoend en is een slecht voorbeeld
+/* Dit word ook wel Object Oriented Syntax genoend en is een slecht voorbeeld
 // Als een object 1 of meer methodes heeft dan noemen we dat behavior(gedrag)
-// const circle = {
-//    radius: 1,
-//    location: {
-//        x: 1, Dit worden in OOP properties oftewel eigenschappen genoemd
-//        y:1
-//    },
-//    draw: function() {  In OOP word een function een method genoemd dus ook in deze instantie is het een method
-//        console.log('draw');
-//    }
-// };
 
+ const circle = {
+    radius: 1,
+    location: {
+        x: 1, Dit worden in OOP properties oftewel eigenschappen genoemd
+        y:1
+    },
+    draw: function() {  In OOP word een function een method genoemd dus ook in deze instantie is het een method
+        console.log('draw');
+    }
+ };
+*/
 // Dit wordt een Factory Function genoemd
 // Als wij de return keyword gebruiken dan heet dat een Factory Function
 function createCircle(radius) {
@@ -33,4 +34,22 @@ function Circle(radius) {
         console.log('draw');
     }
 }
+
+/*
+const Circle1 = new Function('radius', `
+
+
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+`);
+const circle = new Circle1(1);
+*/
+
+
+Circle.call({}, 1);
+Circle.call({}, 1);
+
 const another = new Circle(1);
+
